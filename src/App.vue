5dbcfa61fbfdf982718da190992bfdeaf87f56ec
taiprogrammer <taiza.marques@sptech.school>
2022-05-28 19:23:13 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  list-style: none;
+}
+#app {
+  font-family: "SF Pro Display", "SF Pro Icons", "Helvetica Neue", "Helvetica",
+    "Arial", sans-serif;
+  background: linear-gradient(to right, #1a1a1a 30%, #fff 30%);
+  height: 100vh;
+  padding: 0px 80px;
+}
+</style>
